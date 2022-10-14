4af26b5521c5c4c8c3438c722123b4c1f2846257
Insu Jang <insjang@c7r7s2.42seoul.kr>
2022-10-14 21:00:34 +0900

    insjang solo play

--- srcs/main.c
+++ srcs/main.c
@@ -76,41 +76,104 @@ int	check_border(t_map *map)
 	return (0);
 }
 
-// 미로 세팅 및 map 인자 체크 할 에정
-// void set_map(char *map_dir, t_map *map)
-// {
-// 	int		fd;
-// 	char	*line;
-// 	char	**maze;
+//텍스쳐 경로 map구조체에 저장 made in insjang~~~~~~
+void	save_texture(t_map *map, char *line)
+{
 
-// 	fd = open(map_dir, O_RDONLY);
-// 	if (fd == -1)
-// 		ft_error("File can't open this time\n");
-// 	maze = map->map_info;
-// 	while (1)
-// 	{
-// 		line = get_next_line(fd);
-// 		if (line == NULL)
-// 			break ;
-// 		if (*line == ' ' || *line == '1')
-// 			exit(ft_error("Map is first"));
-// 		else
-// 		{
-// 			if (check_arg(map))
-// 			{
-// 				*maze = malloc(sizeof(char) * (map->w + 1));
-// 				if (*maze == NULL)
-// 					exit(1);
-// 				(*maze)[map->w] = '\0';
-// 				ft_memset(*maze, ' ', map->w);
-// 				ft_memcpy(*maze, line, ft_strlen(line));
-// 				maze++;
-// 			}
-// 			else
-// 				exit(ft_error("Argument Error"));
-// 		}
-// 	}
-// }
+}
+
+//2차원 포인터 개수 구하기 made in insjang~~~~
+int	res_split_cnt(char **sple)
+{
+	int	i;
+
+	if (sple == NULL)
+		return (0);
+	i = 0;
+	while (sple[i])
+		i++;
+	return (i);
+}
+
+//rgb map구조체에 저장 made in insjang~~~~~~
+void	save_rgb(t_map *map, char *line)
+{
+	int		arg_idx;
+	int		i;
+	int		temp;
+	char	**sple;
+	char	c;
+
+	c = *line;
+	line += 1;
+	while (*line == ' ')
+		line++;
+	sple = ft_split(line, ',');
+	if (res_split_cnt(sple) != 3)
+	{
+		free(sple);
+		exit(ft_error("map error"));
+	}
+	i = 0;
+	while (i < 3)
+	{
+		temp = ft_atoi(sple[i]);
+		if (temp < 0 || temp > 255)
+		{
+			free(sple);
+			exit(ft_error("map error"));
+		}
+		if (c == 'F')
+		{
+			map->arg_info[E_F] = 1;
+			map->rgb->floor[i] = temp;
+		}
+		else
+		{
+			map->arg_info[E_C] = 1;
+			map->rgb->ceiling[i] = temp;
+		}
+	
+	}
+
+}
+
+// 미로 세팅 및 map 인자 체크 할 에정
+void set_map(char *map_dir, t_map *map)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(map_dir, O_RDONLY);
+	if (fd == -1)
+		ft_error("File can't open this time\n");
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			exit(ft_error("map error"));
+		if ((!ft_strncmp(line, "NO ", 3) && map->arg_info[E_NO] == 0) || (!ft_strncmp(line, "SO ", 3) && map->arg_info[E_SO] == 0)
+			|| (!ft_strncmp(line, "WE ", 3) && map->arg_info[E_WE] == 0) || (!ft_strncmp(line, "EA ", 3) && map->arg_info[E_EA] == 0))
+			save_texture(map, line);
+		else if ((!ft_strncmp(line, "F ", 2) && map->arg_info[E_F] == 0) || (!ft_strncmp(line, "C ", 2) && map->arg_info[E_C] == 0))
+			save_rgb(map, line);
+		else
+		{
+			if (check_arg(map))
+			{
+				*maze = malloc(sizeof(char) * (map->w + 1));
+				if (*maze == NULL)
+					exit(1);
+				(*maze)[map->w] = '\0';
+				ft_memset(*maze, ' ', map->w);
+				ft_memcpy(*maze, line, ft_strlen(line));
+				maze++;
+			}
+			else
+				exit(ft_error("Argument Error"));
+		}
+	}
+}
 
 // map의 maze를 제외한 인자들 먼저 확인
 void	set_arg(char *map_dir, t_map *map)
@@ -227,6 +290,17 @@ void	set_map_height_width(char *map_dir, t_map *map)
 
 */
 
+//맵 초기화 made in insjang~~~~~~
+void	map_init(t_map *map)
+{
+	map->p_count = 0;
+	ft_memset(map->a_count, 0, sizeof(map->a_count));
+	map->map_info = malloc(sizeof(char *) * (map->h + 1));
+	if (map->map_info == NULL)
+		exit(1);
+	map->map_info[map->h] = NULL;
+}
+
 int	main(int ac, char **av)
 {
 	char	*file_name;
@@ -236,14 +310,9 @@ int	main(int ac, char **av)
 		return (ft_error("Wrong arguments  [./cub3D] [*.cub]"));
 	file_name = av[1];
 	check_extension(file_name);
-	map.p_count = 0;
-	ft_memset(map.a_count, 0, sizeof(map.a_count));
+	set_map(file_name, &map);
 	set_map_height_width(file_name, &map);
-	map.map_info = malloc(sizeof(char *) * (map.h + 1));
-	if (map.map_info == NULL)
-		exit(1);
-	map.map_info[map.h] = NULL;
-	// set_map(file_name, &map);
+	map_init(&map);
 	if (check_border(&map) || check_news(&map))
 		return (ft_error("map error"));
 	printf("Success\n");
